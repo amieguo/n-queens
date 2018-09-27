d45b2cb066e447537d22618ef69bd60e5e67c744
amieguo <amieguo@gmail.com>
2018-09-27 16:28:05 -0700

    completed nqueenssolution

--- src/solvers.js
+++ src/solvers.js
@@ -19,32 +19,83 @@ window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
   var emptyMatrix = window.makeEmptyMatrix(n);
   
-  var addNextRook = function(board) {
-    var matrix = board.rows();
+  // rewrite hasRowConflictAt 
+  // var hasRowConflictAt = function(row) {
+  //   var sumArray = (elem, sum) => elem + sum;      
+  //   return (row.reduce(sumArray) > 1);
+  // }
+  
+  // var hasColConflictAt = function(col) {
+  //   var sumArray = (elem, sum) => elem + sum;      
+  //   return (col.reduce(sumArray) > 1);
+  // }
+  
+  var hasConflictAt = function(arr) {
+    var sumArray = (elem, sum) => elem + sum;      
+    return (arr.reduce(sumArray) > 1);
+  }
+
+  var checkIfValid = function(matrix) {
+    var result = false;
+    
+    for (var i = 0; i < matrix.length; i++) {
+      result = result || hasConflictAt(matrix[i]);
+    }
+    
+    for (var j = 0; j < matrix.length; j++) {
+      let col = [];
+      let sumArray = (elem, sum) => elem + sum; 
+      for (var i = 0; i < matrix.length; i++) {
+        col.push(matrix[i][j]);
+      }
+      result = result || hasConflictAt(col);
+    }
+    
+    return !result;
+  }
+  
+  var findSol = function(matrix) {
+    // loop through all spaces
     for (var i = 0; i < matrix.length; i++) {
       for (var j = 0; j < matrix.length; j++) {
-        if (matrix[i][j] === 0) {
-          var resultBoard = new Board(matrix);
-          resultBoard.togglePiece(i,j);
-          // matrix[i][j] = 1;
-          var stillValid = !(resultBoard.hasAnyRowConflicts() || resultBoard.hasAnyColConflicts());
-          var numPieces = _.reduce(matrix, function(memo, row) {
-            return memo + _.reduce(row, function(memo, col) {
-              return memo + col;
+        // make copy of matrix
+        var copyMatrix = matrix.map(function (arr) {
+          return arr.slice();
+        });
+        // place piece if the space is empty
+        if (copyMatrix[i][j] === 0) {
+          copyMatrix[i][j] = 1;
+          // check if copy of matrix still valid, 
+          var isValid = checkIfValid(copyMatrix);
+          // if it is still valid: 
+          if (isValid) {
+            // if numPieces === n (aka numGood)
+            var numPieces = _.reduce(copyMatrix, function(memo, row) {
+              return memo + _.reduce(row, function(memo, col) {
+                return memo + col;
+              }, 0);
             }, 0);
-          }, 0);
-          if (stillValid && numPieces < matrix.length) {
-            addNextRook(resultBoard);
-          }
-          if (stillValid && numPieces === matrix.length) {
-            solution = resultBoard.rows();
+            if (numPieces === matrix.length) {
+              // return the copy of the matrix
+              return copyMatrix;
+            } else {
+              // else result = helper function on the copy of matrix 
+              var result = findSol(copyMatrix);
+              // if result !== undefined 
+              if (result !== undefined) {
+                // return the result
+                return result;
+              }
+            }
           }
         }
+      // else, no nothing
       }
     }
-    // return resultBoard;
   }
-  addNextRook(new Board(emptyMatrix));
+  
+  solution = findSol(emptyMatrix);
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -63,6 +114,100 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
+  var emptyMatrix = window.makeEmptyMatrix(n);
+  
+  var hasConflictAt = function(arr) {
+    var sumArray = (elem, sum) => elem + sum;      
+    return (arr.reduce(sumArray) > 1);
+  }
+
+  var checkIfValid = function(matrix) {
+    var result = false;
+    
+    // valid row
+    for (var i = 0; i < matrix.length; i++) {
+      result = result || hasConflictAt(matrix[i]);
+    }
+    
+    // valid col
+    for (var j = 0; j < matrix.length; j++) {
+      let col = [];
+      // let sumArray = (elem, sum) => elem + sum; 
+      for (var i = 0; i < matrix.length; i++) {
+        col.push(matrix[i][j]);
+      }
+      result = result || hasConflictAt(col);
+    }
+    
+    // valid diag
+    //  create a diag array storing all diagonals
+    for (var k = -matrix.length + 1; k < matrix.length; k++) {
+      let majorDiag = [];
+      for (var i = 0; i < matrix.length; i++) {
+        var el = matrix[i][k+i];
+        if (el !== undefined) {
+          majorDiag.push(el);
+        }
+      }
+      result = result || hasConflictAt(majorDiag);
+    }
+    
+    for (var k = 0; k < (2 * matrix.length) - 1; k++) {
+      let minorDiag = [];
+      for (var i = 0; i < matrix.length; i++) {
+        var el = matrix[i][k-i];
+        if (el !== undefined) {
+          minorDiag.push(el);
+        }
+      }
+      result = result || hasConflictAt(minorDiag);
+    }    
+    
+    return !result;
+  }
+  
+  var findSol = function(matrix) {
+    // loop through all spaces
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix.length; j++) {
+        // make copy of matrix
+        var copyMatrix = matrix.map(function (arr) {
+          return arr.slice();
+        });
+        // place piece if the space is empty
+        if (copyMatrix[i][j] === 0) {
+          copyMatrix[i][j] = 1;
+          // check if copy of matrix still valid, 
+          var isValid = checkIfValid(copyMatrix);
+          // if it is still valid: 
+          if (isValid) {
+            // if numPieces === n (aka numGood)
+            var numPieces = _.reduce(copyMatrix, function(memo, row) {
+              return memo + _.reduce(row, function(memo, col) {
+                return memo + col;
+              }, 0);
+            }, 0);
+            if (numPieces === matrix.length) {
+              // return the copy of the matrix
+              return copyMatrix;
+            } else {
+              // else result = helper function on the copy of matrix 
+              var result = findSol(copyMatrix);
+              // if result !== undefined 
+              if (result !== undefined) {
+                // return the result
+                return result;
+              }
+            }
+          }
+        }
+      // else, no nothing
+      }
+    }
+  }
+  
+  solution = findSol(emptyMatrix);
+  solution = solution || emptyMatrix;
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -71,7 +216,7 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
+  
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
